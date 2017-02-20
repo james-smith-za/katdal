2d01cd4c419e65b41c9336418502da6751eeeb0b
james-smith-za[ska-dell-laptop-mintRebecca] <smi.james.th@gmail.com>
2017-02-20 11:06:44 +0200

    Temporary hardcode for 6.7 GHz.

--- katdal/h5datav2_5.py
+++ katdal/h5datav2_5.py
@@ -411,7 +411,8 @@ class H5DataV2_5(DataSet):
         # Set up catalogue containing all targets in file, with reference antenna as default antenna
         self.catalogue.add(target.unique_values)
 
-        self.catalogue.antenna = self.sensor['Antennas/%s/antenna' % (self.ref_ant,)][0]
+        #TODO: What is this line of code?
+        #self.catalogue.antenna = self.sensor['Antennas/%s/antenna' % (self.ref_ant,)][0]
         # Ensure that each target flux model spans all frequencies in data set if possible
         self._fix_flux_freq_range()
 
